cd3d9b48a0b247454b2538a46b7425fedee93f84
daruzboy <35991575+daruzboy@users.noreply.github.com>
2019-12-03 11:08:29 +0700

    tugas todo

---
+++ tugas/todo.js
@@ -0,0 +1,62 @@
+let getTodos = () => {
+    const todosJson = localStorage.getItem("todos")
+    const todos = todosJson !== null ? JSON.parse(todosJson) : []
+    return todos
+  }
+  
+  const saveTodos = function(todos) {
+    localStorage.setItem("todos", JSON.stringify(todos))
+  }
+  
+  const completedTodo = () => {
+    const items = document.getElementsByClassName("todo-item")
+    for (let i = 0; i < items.length; i++) {
+      items[i].onclick = function() {
+        const todo = todos.find(todo => {
+                   return todo.text == this.textContent
+        })
+        todo.completed = !todo.completed
+        this.classList.toggle("check")
+        saveTodos(todos);
+        renderTodos(todos, filters)
+      }
+    }
+  }
+
+  completedTodo()
+  const renderTodos = (todos, filters)  => {
+    const filterTodos = todos.filter(function(todo) {
+      const searchText = todo.text
+        .toLocaleLowerCase()
+        .includes(filters.searchText.toLocaleLowerCase())
+      const hideCompleted = !filters.hideCompleted || !todo.completed
+  
+      return searchText && hideCompleted
+    })
+  
+    const incompletedTodos = filterTodos.filter(function(todo) {
+      return !todo.completed
+    })
+  
+    document.querySelector("#todos").innerHTML = ""
+
+    filterTodos.forEach(function(todo) {
+      document.querySelector("#todos").appendChild(generateTodo(todo))
+    })
+    const count = summaryTodo(incompletedTodos)
+    completedTodo()
+    document.getElementById("count").innerHTML =
+      count == 0 ? "Anda enggan punya to do" : count + " item todo"
+  };
+
+  const generateTodo = (todo) => {
+    const p = document.createElement("p")
+    p.textContent = todo.text
+    p.classList.add("todo-item")
+    todo.completed && p.classList.add("check")
+    return p
+  }
+  
+  const summaryTodo = (incompletedTodos) => {
+     return incompletedTodos.length
+  }
